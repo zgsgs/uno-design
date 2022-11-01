3fd704e6647efe3f81ed77fd91370c8d30b8106b
bbk <2087108700@qq.com>
2022-11-02 03:26:59 +0800

    docs: 添加仓库描述信息

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<title>(.*?)<\/title>/,
+        '<title>Title replaced!</title>',
+      )
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), htmlPlugin()],
 })
