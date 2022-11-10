520a3ac3a25f9bb47ab6a9cb5e3701a9e475e9c4
zhao.gaosheng <zhao.gaosheng@nint.com>
2022-11-10 09:50:54 +0800

    feat: 添加模板生成脚手架

---
+++ packages/cli/template/component/index.ts
@@ -0,0 +1,10 @@
+import { install } from '../_utils'
+import <%= displayName %> from './src/<%= name %>.vue'
+
+export const <%= upperPrefix %><%= displayName %> = install(<%= displayName %>)
+
+export type <%= displayName %>Instance = InstanceType<typeof <%= displayName %>>
+
+export * from './src/interface.d'
+
+export default <%= displayName %>
---
+++ packages/cli/template/component/src/__name__.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup name="<%= upperPrefix %><%= displayName %>">
+import { Emits, Props } from './props'
+import type { <%= displayName %>PropsType } from './props'
+
+const prop: <%= displayName %>PropsType = defineProps(Props)
+defineEmits(Emits)
+</script>
+
+<template>
+  <div class="<%= prefix%>-<%= name%>">
+    <%= upperPrefix %><%= displayName %>
+  </div>
+</template>
